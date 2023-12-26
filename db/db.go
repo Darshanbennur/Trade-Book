5e901eaea6d33f5cfc9decad687def5a84f04f48
vaibhav0806 <vaibhav.pandey0806@gmail.com>
2023-12-26 10:51:43 +0530

    add db, .gitignore and env variables

--- db/db.go
+++ db/db.go
@@ -1,1 +1,27 @@
 package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Start(db *gorm.DB) {
+	dsn := os.Getenv("CONN_STR")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database", err)
+	} else {
+		log.Println("database connected successfully")
+	}
+
+	var now time.Time
+	db.Raw("SELECT NOW()").Scan(&now)
+
+	fmt.Println("Current time:", now)
+}
--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import "github.com/Darshanbennur/Trade-Book/cmd"
+import (
+	"log"
+
+	"github.com/Darshanbennur/Trade-Book/cmd"
+	"github.com/Darshanbennur/Trade-Book/db"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+var database gorm.DB
 
 func main() {
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("failed to load environment vairables", err)
+	}
+
+	db.Start(&database)
 	cmd.Execute()
 
 }
